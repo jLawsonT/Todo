f906d5fa9ec992f3662d738c992b567559ec15f4
lawson Turcotte <turcotte.lawson@gmail.com>
2020-10-31 17:28:49 -0600

    First commit

---
+++ functions/index.js
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+const app = require('express')();
+
+const {
+    getAllTodos,
+    postOneTodo,
+    deleteTodo,
+    editTodo
+} = require('./API/todos')
+
+app.get('/todos', getAllTodos);
+app.post('/todos', postOneTodo);
+app.delete('/todos/:todoId', deleteTodo);
+app.put('/todos/:todoId', editTodo);
+exports.api = functions.https.onRequest(app);
+
+
+
+
